7441534f282ff6fccf3ade042908cb52fffec671
jasonwrwilson <jason.wr.wilson@gmail.com>
2021-04-24 15:46:49 -0400

    Simplified Follow Camera added Tile Manager

--- Deeper/Assets/Scripts/CameraController.cs
+++ Deeper/Assets/Scripts/CameraController.cs
@@ -4,13 +4,6 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private Rigidbody2D cameraRigidBody;
-
-    public float moveForce;
-    public float maxMoveSpeed;
-    public float followDistance;
-    public GameObject followTarget;
-
     // Use this for initialization
     void Start()
     {
@@ -20,7 +13,7 @@ public class CameraController : MonoBehaviour
     // Controller is awake
     private void Awake()
     {
-        cameraRigidBody = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
@@ -32,23 +25,8 @@ public class CameraController : MonoBehaviour
     // Physics Update
     private void FixedUpdate()
     {
-        Vector3 followTargetPositionDelta = followTarget.transform.position - transform.position;
-        followTargetPositionDelta.z = 0;
-
-        if (followTargetPositionDelta.magnitude <= followDistance)
-        {
-            cameraRigidBody.velocity = new Vector2(0, 0);
-        }
-        else
-        {
-            Vector2 movementDirection = new Vector2(followTarget.transform.position.x - transform.position.x, followTarget.transform.position.y - transform.position.y);
-
-            cameraRigidBody.AddForce(movementDirection * moveForce);
-
-            if (Mathf.Abs(cameraRigidBody.velocity.magnitude) > maxMoveSpeed)
-            {
-                cameraRigidBody.velocity = cameraRigidBody.velocity.normalized * maxMoveSpeed;
-            }
-        }
+        Vector3 pos = followTarget.transform.position;
+        pos.z = transform.position.z;
+        transform.position = pos;
     }
 }
---
+++ Deeper/Assets/Scripts/TileManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    public DudeController dude;
+
+    public int mapWidth;
+    public int mapHeight;
+    private int[,] tileMap;
+
+    public DeepTile[] tilePrefabs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeMap();
+        InstantiateTiles();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void InitializeMap()
+    {
+        tileMap = new int[mapWidth,mapHeight];
+        for (int i = 0; i < mapWidth; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                tileMap[i,j] = 2;
+            }
+            for (int j = 3; j < mapHeight; j++)
+            {
+                if (i > mapHeight / 2 - 3 && i < mapHeight / 2 + 3)
+                {
+                    tileMap[i,j] = 1;
+                }
+                else
+                {
+                    tileMap[i,j] = 0;
+                }
+            }
+        }
+    }
+
+    private void InstantiateTiles()
+    {
+        for(int i = 0; i < mapWidth; i++)
+        {
+            for(int j = 0; j < mapHeight; j++)
+            {
+                DeepTile newTile = Instantiate(tilePrefabs[tileMap[i,j]], new Vector3(i - mapWidth / 2, -j + 3, 0), Quaternion.identity);
+            }
+        }
+    }
+}
